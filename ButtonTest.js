c73764767129914ffc107aa030fd23c6fc123661
Christian van Drunen <christian.van.drunen@hva.nl>
2025-02-11 12:36:20 +0100

    progress?

--- ButtonTest.js
+++ ButtonTest.js
@@ -1,22 +1,47 @@
+function MyInformation(){
+  var myInformation = document.getElementById("WhoAmIText");
+  myInformation.innerHTML = "Hello, my name is Christian and I am horrible at coding";
+myFunction();
+}
+
+function Leerdoel1(){
+  var myInformation = document.getElementById("WhoAmIText");
+  myInformation.innerHTML = "dus mijn eerste leerdoel is om css te leren";
+  myFunction();
+}
+function Leerdoel2(){
+  var myInformation = document.getElementById("WhoAmIText");
+  myInformation.innerHTML = "En mijn tweede is om te janken";
+  myFunction();
+}
+
 function myFunction() {
-    // var element = document.getElementById("myDIV");
-    // element.classList.add("block");
-    var element2 = document.getElementById("myButton");
-    element2.hidden = true;
+    var showInformation = document.getElementById("AllButtons");
+    showInformation.hidden = true;
+
     createBlocks();
+    var myInformation = document.getElementById("WhoAmIText");
+    myInformation.classList.remove("hide");
+    
+    myInformation.hidden = false;
+
+    var removeBlocksButton = document.getElementById("removeBlocks");
+    removeBlocksButton.classList.remove("hide");
+    removeBlocksButton.hidden = false;
   }
 
+let blockTimeouts = [];
 
 function createBlocks() {
       const blockSize = 100;
-      const cols = Math.ceil(window.innerWidth / blockSize); // Aantal kolommen
-      const rows = Math.ceil(window.innerHeight / blockSize); // Aantal rijen
+      const cols = Math.ceil(window.innerWidth / blockSize);
+      const rows = Math.ceil(window.innerHeight / blockSize);
 
-      const blocks = []; // Array van blokken
+      const blocks = [];
 
       // Vul de array met blokken
       for (let i = 0; i < rows * cols; i++) {
-        blocks.push(i);  // We voegen de index van het blok toe
+        blocks.push(i);
       }
 
       // Gebruik forEach om elk blok te plaatsen
@@ -33,10 +58,26 @@ function createBlocks() {
           block.style.left = `${left}px`;
           block.style.top = `${top}px`;
     
-          // Voeg het blok toe aan de body
-          document.body.appendChild(block);
-        }, blockIndex * 30); // 1 seconde vertraging per blok
+          // make the block a child
+          document.getElementById('block-container').appendChild(block);
+        }, blockIndex * 20);
       });
     }
 
-    // Roep de functie aan om de blokken te maken
+    function removeBlocks() {
+      blockTimeouts.forEach(clearTimeout);
+      blockTimeouts = [];
+      document.getElementById('block-container').innerHTML = '';
+
+      
+      var showInformation = document.getElementById("AllButtons");
+      showInformation.hidden = false;
+
+      var removeBlocksButton = document.getElementById("removeBlocks");
+      removeBlocksButton.hidden = true;
+
+      var myInformation = document.getElementById("WhoAmIText");
+      myInformation.hidden = true;
+    }
+
+  
--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,12 @@
 https://www.youtube.com/watch?v=o_hNQQBYpeE
+
+
+https://support.glitch.com/t/using-javascript-to-show-and-hide-text/41775/3
+
+https://www.w3schools.com/howto/howto_css_button_group.asp
+
+lego blokje voor achtergrond
+https://codepen.io/cassidoo/pen/xgKqNe?utm_source=extension&utm_medium=click&utm_campaign=muzli
+
+adding brick class when pressing on button
+https://www.w3schools.com/howto/howto_js_add_class.asp
